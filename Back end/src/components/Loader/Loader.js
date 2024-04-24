b7fa473053082b1b0a3f6a677c223cc2124d70e2
Sruthakeerthy <sruthakeerthydasoju@gmail.com>
2024-04-23 21:16:09 -0400

    project

---
+++ Back end/src/components/Loader/Loader.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+// Keyframes for the loader animation
+const spin = keyframes`
+  0% {
+    box-shadow: .2em 0px 0 0px currentcolor;
+  }
+  12% {
+    box-shadow: .2em .2em 0 0 currentcolor;
+  }
+  25% {
+    box-shadow: 0 .2em 0 0px currentcolor;
+  }
+  37% {
+    box-shadow: -.2em .2em 0 0 currentcolor;
+  }
+  50% {
+    box-shadow: -.2em 0 0 0 currentcolor;
+  }
+  62% {
+    box-shadow: -.2em -.2em 0 0 currentcolor;
+  }
+  75% {
+    box-shadow: 0px -.2em 0 0 currentcolor;
+  }
+  87% {
+    box-shadow: .2em -.2em 0 0 currentcolor;
+  }
+`;
+
+// Styled component for the loader
+const LoaderContainer = styled.span`
+  display: inline-block;
+  transform: rotateZ(45deg);
+  perspective: 1000px;
+  border-radius: 50%;
+  width: 48px;
+  height: 48px;
+  color: #fff;
+
+  &:before,
+  &:after {
+    content: '';
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: inherit;
+    height: inherit;
+    border-radius: 50%;
+    transform: rotateX(70deg);
+    animation: 1s ${spin} linear infinite;
+  }
+
+  &:after {
+    color: #FF3D00;
+    transform: rotateY(70deg);
+    animation-delay: .4s;
+  }
+`;
+
+const Loader = () => {
+  return <LoaderContainer />;
+};
+
+export default Loader;
